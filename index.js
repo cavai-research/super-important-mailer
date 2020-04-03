4d55b0050c3c6b3c00f8d842207c6d6f14ca7005
Margus Kevin Sünter <kevin@synter.ee>
2020-04-03 16:27:59 +0300

    Update index.js with randomness

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const ImageLoader = require('./ImageLoader');
 const Mailgun = require('./Mailgun');
 
-class Mailer {
-  /**
-   * Sends out emails based on template
-   */
-  static async sendMail() {
-    const imageLink = ImageLoader.getImageUrl();
 
-    let html = await fs.readFile(path.resolve(__dirname, `template.html`), 'utf-8');
-    html = html.replace(`{{ imageLink }}`, imageLink);
+/**
+ * Sends out emails based on template
+ */
+function sendMail() {
+  const imageLink = ImageLoader.getImageUrl();
 
-    return Mailgun.send({
-      subject: 'Super important action required!',
-      html
-    })
-  }
+  let html = fs.readFileSync(path.resolve(__dirname, `template.html`), 'utf-8');
+  html = html.replace(`{{ imageLink }}`, imageLink);
+
+  return Mailgun.send({
+    subject: 'Super important action required!',
+    html
+  });
 }
 
-(async () => {
-  await Mailer.sendMail()
-    .catch(error => {
-      console.log(error);
-    })
-    .then(() => {
-      console.log('Successfully sent');
-    })
-})()
+/**
+ * Get random Date in future
+ *
+ * @returns {Date}
+ */
+function getRandomTime() {
+  // Configure randomness
+  // Config next date range
+  const minDays = 7;
+  const maxDays = 14;
+  // Config time range, fromm morning to evening
+  const minHours = 8;
+  const maxHours = 17;
+
+  const currentDate = new Date();
+
+  // Get next random day
+  const randomDay = Math.floor(Math.random() * (maxDays - minDays + 1) + minDays);
+  // Get next random working hour
+  const randomHour = Math.floor(Math.random() * (maxHours - minHours + 1) + minHours);
+
+  return new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate() + randomDay,
+    randomHour
+  );
+}
+
+/**
+ * Queue mail sending
+ */
+function queue() {
+  if (process.env.IMMEDIATE) {
+    sendMail();
+  }
+  setTimeout(() => {
+    sendMail();
+    queue();
+  }, getRandomTime().getTime() - Date.now());
+}
+
+queue();
